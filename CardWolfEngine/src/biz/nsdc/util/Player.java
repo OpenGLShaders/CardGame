3b083965f122ef32a635ed142816fcc37f6149ad
Sam Kaessner <openglshaders@gmail.com>
2016-03-24 16:06:10 -0600

    Added a single argument constructor for the player class.

--- CardWolfEngine/src/biz/nsdc/util/Player.java
+++ CardWolfEngine/src/biz/nsdc/util/Player.java
@@ -10,6 +10,9 @@ public class Player {
 	protected Deck<? extends Card> hand;
 	protected String name;
 	
+	public Player(String name) {
+		this(name, null);
+	}
 	
 	/**
 	 * Holds a player. Takes the name of the player and his hand of cards. 
